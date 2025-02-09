4125701d6df02ba87c809a99f85938704b6f4d15
rxsOwO <91576107+rxsOwO@users.noreply.github.com>
2025-02-09 14:49:07 -0500

    Player and Place

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    //public bool active;
+    public float moveSpeed = 5f;
+    public Rigidbody2D rb;
+    Vector2 movement;
+    public Animator animator;
+    void Update() 
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        /*animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.magnitude);*/
+    }
+    void FixedUpdate()
+    {
+        //if(active) {
+            rb.MovePosition(rb.position + movement * moveSpeed * Time.deltaTime);
+        //}
+    }
+}
